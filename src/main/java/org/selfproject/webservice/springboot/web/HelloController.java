3228878b4ec67d733a0a2da7584d781f81a87a05
supra1nova <supra1nova@gmail.com>
2022-09-24 16:11:27 +0900

    #4 롬복을 이용한 테스트 코드 작성 롬복 디펜던시 설정 뒤 테스트 코드 작성. JUnit4, assertJ 이용 테스트 진행.

--- src/main/java/org/selfproject/webservice/springboot/web/HelloController.java
+++ src/main/java/org/selfproject/webservice/springboot/web/HelloController.java
@@ -1,13 +1,20 @@
 package org.selfproject.webservice.springboot.web;
 
+import org.selfproject.webservice.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam(name = "name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
